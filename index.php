8e126823753a4284f4007da190958673f18bcb05
Trang Luu <tluu9@mail.greenriver.edu>
2019-04-12 16:35:42 -0400

    Added view to index

--- index.php
+++ index.php
@@ -1,20 +1,30 @@
 <?php
+/** Another pair programming project...*/
+/**Trang Luu
+ *12 April 2019
+ * Controller page
+ */
 
 //Turn on error reporting
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
-//require the autoload file
+//require the autoload file autoload.php
 require_once('vendor/autoload.php');
 
-//Create an instance of the Base class
+//Create an instance of the Base class/ instantiate Fat-Free
 $f3 = Base::instance();
 
-//Define a default route
+//Turn on Fat-free error reporting/Debugging
+$f3->set('DEBUG',3);
+
+//Define a default route (use backlash / )
 $f3->route('GET /', function()
 {
-echo'<h1>Pet Home</h1>';
+    //Display a view-set view as new template and echo out the view
+    $view = new Template();
+    echo $view->render('views/home.html');
 });
 
-//Run fat free
+//Run fat free F3
 $f3->run();
